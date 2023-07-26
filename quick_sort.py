d82bf7de44820a88ec6dc14357c04686f16c521b
benwu232 <benwu232@163.com>
2023-07-26 22:49:02 +1200

    add quicksort

---
+++ quick_sort.py
@@ -0,0 +1,22 @@
+
+def quick_sort(a, start, end):
+    if start >= end:
+        return
+    pivot = a[end]
+    pivot_idx = start
+    for k in range(start, end):
+        if a[k] <= pivot:
+            a[pivot_idx], a[k] = a[k], a[pivot_idx]
+            pivot_idx += 1
+    a[pivot_idx], a[end] = a[end], a[pivot_idx]
+    quick_sort(a, start, pivot_idx-1)
+    quick_sort(a, pivot_idx+1, end)
+
+
+
+if __name__ == '__main__':
+    a = [1, 9,8,7,6,5,4,3,2,1,0]
+    print(a)
+
+    quick_sort(a, 0, len(a)-1)
+    print(a)

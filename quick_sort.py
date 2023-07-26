5bc3456bc990b19deef4f98c20cb57596a99e186
benwu232 <benwu232@163.com>
2023-07-26 23:02:52 +1200

    update quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -8,15 +8,25 @@ def quick_sort(a, start, end):
         if a[k] <= pivot:
             a[pivot_idx], a[k] = a[k], a[pivot_idx]
             pivot_idx += 1
-    a[pivot_idx], a[end] = a[end], a[pivot_idx]
+    a[end] = a[pivot_idx]
+    a[pivot_idx] = pivot
+    # a[pivot_idx], a[end] = a[end], a[pivot_idx]
     quick_sort(a, start, pivot_idx-1)
     quick_sort(a, pivot_idx+1, end)
 
 
 
 if __name__ == '__main__':
-    a = [1, 9,8,7,6,5,4,3,2,1,0]
-    print(a)
+    # a = [1, 9,8,7,6,5,4,3,2,1,0]
+    # print(a)
+    a = [
+        [],
+        [1],
+        [1, 2, 3, 4, 5],
+        [5, 4, 3, 2, 1],
+        [5, 4, 3, 2, 1, 5, 4, 3, 2, 1]
+    ]
 
-    quick_sort(a, 0, len(a)-1)
-    print(a)
+    for l in a:
+        quick_sort(l, 0, len(l)-1)
+        print(l)
